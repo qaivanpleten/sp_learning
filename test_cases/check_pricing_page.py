863add9c385e8cc993860b7c329fd1b3272d0324
Your Name <you@example.com>
2017-09-12 14:35:42 +0300

    first commint

--- test_cases/check_pricing_page.py
+++ test_cases/check_pricing_page.py
@@ -1,3 +1,5 @@
+import time
+
 from actions.general_actions import GeneralActions
 from actions.setup_class import SetUpClass
 from elements.main_page_elements import MainPageElements
@@ -5,23 +7,24 @@ from elements.pricing_elements import PricingPageElements
 
 
 class CheckFaqPage(SetUpClass):
-    def test_general_elements(self):
-        general_action = GeneralActions(self.driver)
-        mp_element = MainPageElements(self.driver)
-        general_action.open_page_by_url('http://console.dev.sonikpass.com/#pricing')
-
-        # check header elements
-        general_action.check_element_on_page(mp_element.logo())
-        general_action.check_element_on_page(mp_element.signup_button())
-        general_action.check_element_on_page(mp_element.login_button())
-        general_action.check_element_on_page(mp_element.hamburger_menu_button())
-
-        # check footer elements
-        general_action.check_element_on_page(mp_element.footer_whyus())
-        general_action.check_element_on_page(mp_element.footer_company())
-        general_action.check_element_on_page(mp_element.footer_career())
-        general_action.check_element_on_page(mp_element.footer_faq())
-        general_action.check_element_on_page(mp_element.footer_contact())
+    # def test_general_elements(self):
+    #     general_action = GeneralActions(self.driver)
+    #     mp_element = MainPageElements(self.driver)
+    #     general_action.open_page_by_url('http://console.dev.sonikpass.com/#pricing')
+    #     time.sleep(10)
+    #
+    #     # check header elements
+    #     general_action.check_element_on_page(mp_element.logo())
+    #     general_action.check_element_on_page(mp_element.signup_button())
+    #     general_action.check_element_on_page(mp_element.login_button())
+    #     general_action.check_element_on_page(mp_element.hamburger_menu_button())
+    #
+    #     # check footer elements
+    #     general_action.check_element_on_page(mp_element.footer_whyus())
+    #     general_action.check_element_on_page(mp_element.footer_company())
+    #     general_action.check_element_on_page(mp_element.footer_career())
+    #     general_action.check_element_on_page(mp_element.footer_faq())
+    #     general_action.check_element_on_page(mp_element.footer_contact())
 
     def test_elements(self):
         general_action = GeneralActions(self.driver)
