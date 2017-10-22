4fe88c8b4c16db7e83ce0c787bd34f32f64972ff
qaivanpleten <chesteslav@gmail.com>
2017-10-22 10:37:29 +0300

    Add page object for AboutUs page. Create main_elements and about_us_page files with objects. Change test_about_us_page using page object.

---
+++ sp_at/pages/main_elements.py
@@ -0,0 +1,24 @@
+from selenium.webdriver.remote.webdriver import WebDriver
+
+
+class BasePage(object):
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+
+class MainElements(BasePage):
+    def present_header_elements(self):
+        logo = WebDriver.find_element_by_xpath(self, '//*[@id="app-container"]/a/div[2]')
+        signup_button = self.find_element_by_id('signup_link')
+        login_button = self.driver.find_element_by_xpath('//*[@id="app-container"]/div[2]/button')
+        hamburger_menu_button = self.driver.find_element_by_xpath('//*[@id="app-container"]/div[1]/div')
+
+        for elements in (logo, signup_button, login_button, hamburger_menu_button):
+            if elements.is_displayed():
+                return True
+
+            else:
+                return False
+
+    def general_url(self):
+        return 'http://console.sonikpass.com/'
