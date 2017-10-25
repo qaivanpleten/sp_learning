a7088bc2a98f5e56b24957e3139e03a6814e99d7
qaivanpleten <chesteslav@gmail.com>
2017-10-25 08:50:15 +0300

    Deleted unused imports. Fix issue with incorrect return in "present" function

--- sp_at/pages/integration_page.py
+++ sp_at/pages/integration_page.py
@@ -1,6 +1,5 @@
 from abc import ABC, abstractmethod
 
-import time
 from selenium.webdriver.remote.webdriver import WebDriver
 
 from sp_at.elements.element import ElementByXpath, ElementById
@@ -16,12 +15,11 @@ class _Body(Body):
         self._schedule_block = ElementById(driver, 'content_left')
         self._schedule_button = ElementByXpath(driver, '//*[@id="content_right"]/a/button')
 
-    def present(self)-> bool:
+    def present(self) -> bool:
         for element in (self._title, self._what_next_block, self._schedule_block, self._schedule_button):
-            if element.instance().is_displayed():
-                return True
-            else:
+            if not element.instance().is_displayed():
                 return False
+        return True
 
 
 class IntegrationPage(ABC):
@@ -53,21 +51,3 @@ class InitialIntegrationPage(IntegrationPage):
 
     def body(self) -> Body:
         return self._body
-
-
-class SlowIntegrationPage(IntegrationPage):
-    def __init__(self, page: IntegrationPage):
-        self._page = page
-
-    def open(self):
-        self._page.open()
-        time.sleep(5)
-
-    def body(self) -> Body:
-        time.sleep(3)
-        return self._page.body()
-
-    def header(self) -> Header:
-        time.sleep(3)
-        return self._page.header()
-
--- sp_at/test_cases/test_integration_page.py
+++ sp_at/test_cases/test_integration_page.py
@@ -1,9 +1,6 @@
 import allure
 import pytest
 
-from sp_at.actions.general_actions import GeneralActions
-from sp_at.elements.integration_page_elements import IntegrationPageElements
-from sp_at.elements.main_page_elements import MainPageElements
 from sp_at.pages.integration_page import InitialIntegrationPage
 
 
@@ -14,6 +11,7 @@ def test_general_elements(fixture_webdriver):
     page = InitialIntegrationPage(fixture_webdriver)
     page.open()
     assert page.header().present(), "Header is brocken: some element isn't displayed"
+
 
 @pytest.allure.severity(pytest.allure.severity_level.NORMAL)
 @allure.feature('Check elements on Integration page')
