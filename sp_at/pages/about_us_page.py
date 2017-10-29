8eda3f8695869fe87e0fa1a12cb01aca7d4e80e1
qaivanpleten <chesteslav@gmail.com>
2017-10-29 14:54:11 +0200

    edit test and page_object_file for AboutUs page using page object rules

--- sp_at/pages/about_us_page.py
+++ sp_at/pages/about_us_page.py
@@ -1,59 +1,81 @@
 import time
+from abc import ABC, abstractmethod
+
 from selenium.webdriver.remote.webdriver import WebDriver
 
-from sp_at.pages.main_elements import MainElements
+from sp_at.elements.element import ElementByXpath
+from sp_at.elements.url import SimpleUrl, AddressOf
+from sp_at.pages import main_url
+from sp_at.pages.structures import Body, Header, MainHeader
 
 
-# class BasePage(object):
-#     def __init__(self, driver: WebDriver):
-#         self.driver = driver
-
-
-class AboutUsPage:
+class _Body(Body):
     def __init__(self, driver: WebDriver):
-        self.driver = driver
+        self._title = ElementByXpath(driver, '//*[@id="sub-region"]/div/section/div[1]/div/h1')
+        self._meet_the_team_title = ElementByXpath(driver, '//*[@id="sub-region"]/div/section/div[2]/div/h1')
+        self._open_positions_button = ElementByXpath(driver, '//*[@id="sub-region"]/div/section/div[8]/div/p/a[1]/button')
+        self._get_in_touch_button = ElementByXpath(driver, '//*[@id="sub-region"]/div/section/div[8]/div/p/a[2]/button')
+
+        # xpath_selector = 3
+        # self._portfolio_block = ElementByXpath(driver, '//*[@id="sub-region"]/div/section/div[' + str(xpath_selector) + ']')
+        # self._portfolio_block = ElementByXpath('//*[@id="sub-region"]/div/section/div[3]')
+
+
+    # elements are displayed on the page
+
+    def present(self) -> bool:
+        for elements in (
+        self._title, self._meet_the_team_title, self._open_positions_button, self._get_in_touch_button):
+            if not elements.instance().is_displayed():
+                return False
+            else:
+                return True
+
+                # while xpath_selector <= 8:
+                #     xpath_selector = 3
+                #     portfolio_block = self.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[' + str(xpath_selector) + ']')
+                #     if portfolio_block.is_displayed():
+                #         return True
+                #     else:
+                #         return False
+                #
+                #     xpath_selector += 1
+
+                # def open_position_button_work(self):
+                #     open_position_button = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[8]/div/p/a[1]/button')
+                #     open_position_button.click()
+                #     time.sleep(3)
+                #
+                #     if MainElements.general_url(self) + '#careers' in self.driver.current_url:
+                #         return True
+                #     else:
+                #         return False
+
+
+class AboutUsPage(ABC):
+    @abstractmethod
+    def open(self):
+        pass
+
+    def header(self) -> Header:
+        pass
+
+    def body(self) -> Body:
+        pass
+
+
+class InitialAboutUsPage(AboutUsPage):
+    def __init__(self, driver: WebDriver):
+        self._header = MainHeader(driver)
+        self._browser = driver
+        self._url = SimpleUrl(driver, AddressOf(main_url, '#about'))
+        self._body = _Body(driver)
 
     def open(self):
-        self.driver.get(MainElements.general_url(self) + '#about')
-        assert MainElements.general_url(self) + '#about' in self.driver.current_url
+        self._url.open()
 
-    def present_body_elements(self):
-        xpath_selector = 3
-        title = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[1]/div/h1')
-        meet_the_team_title = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[2]/div/h1')
-        #portfolio_block = self.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[' + xpath_selector + ']')
-        #portfolio_block = self.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[5]')
-        open_positions_button = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[8]/div/p/a[1]/button')
-        get_in_touch_button = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[8]/div/p/a[2]/button')
+    def header(self) -> Header:
+        return self._header
 
-        for elements in (title, meet_the_team_title, open_positions_button, get_in_touch_button):
-            if elements.is_displayed():
-                return True
-            else:
-                return False
-
-
-        # while xpath_selector <= 8:
-        #     xpath_selector = 3
-        #     portfolio_block = self.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[' + str(xpath_selector) + ']')
-        #     if portfolio_block.is_displayed():
-        #         return True
-        #     else:
-        #         return False
-        #
-        #     xpath_selector += 1
-
-
-
-
-    def open_position_button_work(self):
-        open_position_button = self.driver.find_element_by_xpath('//*[@id="sub-region"]/div/section/div[8]/div/p/a[1]/button')
-        open_position_button.click()
-        time.sleep(3)
-
-        if MainElements.general_url(self) + '#careers' in self.driver.current_url:
-            return True
-        else:
-            return False
-
-
+    def body(self) -> Body:
+        return self._body
